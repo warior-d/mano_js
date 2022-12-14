a72f26ab86e4ed9adaff2091d0bcfda180103996
Dmitry <dakornev1@mts.ru>
2022-12-14 06:06:47 +0000

    ...ok, but not now.. 11 stage

--- core/engine.php
+++ core/engine.php
@@ -28,6 +28,115 @@ define('GET_LCM_STATUS', '/nslcm/v1/ns_lcm_op_occs/');
 }
 
 
+function getOpenstackNetworks()
+{
+	
+	include "jsons.php";
+	
+	$api_openstack = $_REQUEST['api_openstack'];
+	$openstack_user = $_REQUEST['openstack_user'];
+	$openstack_pass = $_REQUEST['openstack_pass'];
+	$openstack_tenant = $_REQUEST['openstack_tenant'];
+	
+	
+	$os_token_url = '/auth/tokens';
+	
+	$json_os = getOStoken($openstack_user, $openstack_pass, $openstack_tenant);
+
+	$headers = [];
+	$ch = curl_init($api_openstack.$os_token_url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+	'Accept: application/json'));
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $json_os); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
+	curl_setopt($ch, CURLOPT_HEADERFUNCTION,
+		function($curl, $header) use (&$headers)
+	{
+		$len = strlen($header);
+		$header = explode(':', $header, 2);
+		if (count($header) < 2) // ignore invalid headers
+		  return $len;
+
+		$headers[strtolower(trim($header[0]))][] = trim($header[1]);
+		
+		return $len;
+	  }
+	);
+	
+	$arr_token = curl_exec($ch);
+	curl_close($ch);
+
+	
+	$response_array = json_decode($arr_token, true);
+	$token_os_arr = $headers['x-subject-token'];
+	$out_x_token = $token_os_arr;
+	$x_token = $token_os_arr[0];
+	
+	$project_id = $response_array['token']['project']['id'];
+	$neutron_api_url = '';
+	
+	for($i = 0; $i < count($response_array['token']['catalog']); $i++){
+		
+		$inner_arr = $response_array['token']['catalog'][$i];
+		
+		if($inner_arr['type'] == 'network'){
+			$neutron_api_url = $inner_arr['endpoints']['0']['url'];
+		}
+		
+	}
+	
+	$url_get_nets = $neutron_api_url.'/v2.0/networks?project_id='.$project_id;
+
+	$ch = curl_init($url_get_nets);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+	'X-Auth-Token: '.$x_token));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
+	$res = curl_exec($ch);
+	curl_close($ch);
+
+	$decoded_json_nets = json_decode($res, true);
+	$nets = $decoded_json_nets['networks'];
+	$new_obj = [];
+
+	for($i = 0; $i < count($nets); $i++){
+		foreach($nets[$i] as $key=>$value){
+			if($key == 'name'){
+				array_push($new_obj, $value);
+			}
+		}
+	}
+	
+    echo json_encode($new_obj);	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function delACL()
 {
--- core/test.php
+++ core/test.php
@@ -14,145 +14,96 @@ define('GET_VNFDS', '/vnfpkgm/v1/vnf_packages');
 
 
 
-$a = getInstancesAndVims("LkxAJRv6gDFEd9pI8ca2ioNcLOYHFhYS");
-print_r($a);
+
+$a = getOpenstackNetworks();
 
 
-function getInstancesAndVims($token)
+
+
+
+function getOpenstackNetworks()
 {
+	
+	include "jsons.php";
+	
+	$api_openstack = 'http://10.0.69.114/identity/v3';
+	$openstack_user = 'admin';
+	$openstack_pass = 'devstack';
+	$openstack_tenant = 'admin';
+	
+	$os_token_url = '/auth/tokens';
+	
+	$json_os = getOStoken($openstack_user, $openstack_pass, $openstack_tenant);
 
-
-
-	$ch = curl_init(API_MANO_BASE.GET_VNFDS);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-	'Accept: application/json; charset=utf-8', 'Connection: keep-alive', 'Authorization: Bearer '.$token));
+	$headers = [];
+	$ch = curl_init($api_openstack.$os_token_url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+	'Accept: application/json'));
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $json_os); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-	$res_vnfd = curl_exec($ch);
-	curl_close($ch);
-	#print_r($res_vnfd);
-	$decoded_json_vnfd = json_decode($res_vnfd, true);
-	#print_r($decoded_json_vnfd);
-	
-	$arrVnfds = [];
-	$needed_vnfd_params = ["_id","name","id","vdu"];
-	$need_vdu_params = ["id","cloud-init"];
-	$arrVdus = [];
-	
-	
-	for($i = 0; $i < count($decoded_json_vnfd); $i++){
-		foreach($decoded_json_vnfd[$i] as $key=>$value){
-			if(in_array($key, $needed_vnfd_params)){
-				if($key == "vdu"){
-					for($g = 0; $g < count($decoded_json_vnfd[$i][$key]); $g++){
-						foreach($decoded_json_vnfd[$i][$key][$g] as $key_vdu=>$value_vdu){
-							if(in_array($key_vdu, $need_vdu_params)){
-								$arrVnfds[$i][$key][$g][$key_vdu] = $value_vdu;
-								if($key_vdu == 'cloud-init'){
-									$str_json = json_encode($value_vdu);
-									$arrVnfds[$i][$key][$g]['password'] = $str_json.strpos($str_json, 'password:');
-									
-									if(strpos($str_json, 'password:') > 0){
-										$arrVnfds[$i][$key][$g]['password'] = 'truhno';
-										$start = strpos($str_json, 'password:');
-										$str = substr($str_json, $start + strlen('password: '), strpos($str_json, '\n', $start) - $start - strlen('password: '));
-										$arrVnfds[$i][$key][$g]['password'] = $str;
-										$arrVnfds[$i]['password'] = $str;
-									}
-								}
-							}
-						}
-					}
-				}
-				else{
-					$arrVnfds[$i][$key] = $value;
-				}
-			}
-		}
-	}
+	curl_setopt($ch, CURLOPT_HEADERFUNCTION,
+		function($curl, $header) use (&$headers)
+	{
+		$len = strlen($header);
+		$header = explode(':', $header, 2);
+		if (count($header) < 2) // ignore invalid headers
+		  return $len;
 
-
-	$ch = curl_init(API_MANO_BASE.GET_VIMS);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-	'Accept: application/json; charset=utf-8', 'Connection: keep-alive', 'Authorization: Bearer '.$token));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-	$res = curl_exec($ch);
-	curl_close($ch);
-	#print_r($res);
-	$decoded_json = json_decode($res, true);
-	#print_r( $decoded_json);
-	
-	$arrVims = [];
-	$needed_vim_params = ["_id","name","vim_url","resources","vim_tenant_name"];
-	
-	for($i = 0; $i < count($decoded_json); $i++){
-		foreach($decoded_json[$i] as $key=>$value){
-			if(in_array($key, $needed_vim_params)){
-				$arrVims[$i][$key] = $value;
-			}
-		}
-	}
-
-
-	$ch = curl_init(API_MANO_BASE.GET_RUNNING_INSTANCES);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-	'Accept: application/json; charset=utf-8', 'Connection: keep-alive', 'Authorization: Bearer '.$token));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
-	$res = curl_exec($ch);
-	curl_close($ch);
-	$decoded_json_nss = json_decode($res, true);
-	
-	for($i = 0; $i < count($decoded_json_nss); $i++){
-		foreach($decoded_json_nss[$i] as $key=>$value){
-			if($key == "datacenter"){
-				for($y = 0; $y < count($arrVims); $y++){
-					foreach($arrVims[$y] as $k=>$v){
-						if(($k == '_id') && $v == $value){
-							
-								$decoded_json_nss[$i]["datacenter_name"] = $arrVims[$y]["name"];
-								$decoded_json_nss[$i]["vim_resources"] = $arrVims[$y]["resources"];
-								$decoded_json_nss[$i]["vim_url"] = $arrVims[$y]["vim_url"];
-								$decoded_json_nss[$i]["vim_tenant"] = $arrVims[$y]["vim_tenant_name"];
-						}
-					}
-				}
-			}
-			else if($key ==  "vnfd-id"){
-				
-				for($j = 0; $j < count($decoded_json_nss[$i][$key]); $j++){
-					$arrNS = [];
-					$curr_vnfd_id = $decoded_json_nss[$i][$key][$j];
-					print_r($curr_vnfd_id);
-						for($k = 0; $k < count($arrVnfds); $k++){
-								
-								if($curr_vnfd_id == $arrVnfds[$k]["_id"]){
-									
-									$arrNS["_id"] = $curr_vnfd_id;
-									if(array_key_exists("password", $arrVnfds[$k])){
-										$arrNS["password"] = $arrVnfds[$k]["password"];
-									}									
-									
-									
-									
-									$decoded_json_nss[$i][$key][$j] = $arrNS;
-
-									
-								}
-							
-						}
-				}
-			}
-		}		
-	}
+		$headers[strtolower(trim($header[0]))][] = trim($header[1]);
 		
-	$json_out = json_encode($decoded_json_nss);
-	print_r($decoded_json_nss);
+		return $len;
+	  }
+	);
+	
+	$arr_token = curl_exec($ch);
+	curl_close($ch);
 
+	
+	$response_array = json_decode($arr_token, true);
+	$token_os_arr = $headers['x-subject-token'];
+	$out_x_token = $token_os_arr;
+	$x_token = $token_os_arr[0];
+	
+	$project_id = $response_array['token']['project']['id'];
+	$neutron_api_url = '';
+	
+	for($i = 0; $i < count($response_array['token']['catalog']); $i++){
+		
+		$inner_arr = $response_array['token']['catalog'][$i];
+		
+		if($inner_arr['type'] == 'network'){
+			$neutron_api_url = $inner_arr['endpoints']['0']['url'];
+		}
+		
+	}
+	
+	$url_get_nets = $neutron_api_url.'/v2.0/networks';
+
+	$ch = curl_init($url_get_nets);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json',
+	'X-Auth-Token: '.$x_token));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HEADER, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)");
+	$res = curl_exec($ch);
+	curl_close($ch);
+
+	$decoded_json_nets = json_decode($res, true);
+	$nets = $decoded_json_nets['networks'];
+	$new_obj = [];
+
+	for($i = 0; $i < count($nets); $i++){
+		foreach($nets[$i] as $key=>$value){
+			if($key == 'name'){
+				array_push($new_obj, $value);
+			}
+		}
+	}
+	
+    echo json_encode($new_obj);	
 }
 
 
